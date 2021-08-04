63afa66c58eda5aeb36f0ea022de3b556aadea0e
Joe Bollinger <joeboo865@gmail.com>
2021-08-04 08:53:14 -0400

    optimised for lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,42 +1,24 @@
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { AuthComponent } from "./auth/auth.component";
-import { AuthGuard } from "./auth/auth.guard";
-
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipesResolverService } from "./recipes/recipes-resolver.service";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "/recipes", pathMatch: "full" },
   {
     path: "recipes",
-    component: RecipesComponent,
-    canActivate: [AuthGuard],
-    children: [
-      { path: "", component: RecipeStartComponent },
-      { path: "new", component: RecipeEditComponent },
-      {
-        path: ":id",
-        component: RecipeDetailComponent,
-        resolve: [RecipesResolverService],
-      },
-      {
-        path: ":id/edit",
-        component: RecipeEditComponent,
-        resolve: [RecipesResolverService],
-      },
-    ],
+    loadChildren: "./recipes/recipes.module#RecipesModule",
+    // loadChildren: () => import("./recipes/recipes.module").then(module => module.RecipesModule)  // --- alternate syntax ---
   },
-  { path: "shopping-list", component: ShoppingListComponent },
-  { path: "auth", component: AuthComponent },
+  {
+    path: "shopping-list",
+    loadChildren: "./shopping-list/shopping-list.module#ShoppingListModule",
+  },
+  { path: "auth", loadChildren: "./auth/auth.module#AuthModule" },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Subscription } from "rxjs";
+import { LoggingService } from "../logging.service";
+
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "./shopping-list.service";
 
@@ -12,7 +14,10 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
   private igChangeSub: Subscription;
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(
+    private slService: ShoppingListService,
+    private loggingService: LoggingService
+  ) {}
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
@@ -20,6 +25,9 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
+    );
+    this.loggingService.printLog(
+      "Hello from shopping-list component ngOnInit()"
     );
   }
 
